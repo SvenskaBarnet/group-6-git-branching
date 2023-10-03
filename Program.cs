ebfb1a2a40d391883784e5cfd94118d86642d1e1
= <klas_hermansson@hotmail.com>
2023-10-03 14:46:20 +0200

    Created new branch for input function
    Created a function for entering name and having the console printing it.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+Console.Write("Write your name please: ");
+string name = Console.ReadLine();
+Console.WriteLine(name);
